5931527832c5433aac2f934234538cb7431546b6
lapyiu <lapyiu@outlook.com>
2021-12-20 19:19:08 +0800

    01day-squash

---
+++ expanding-cards/vue/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="HelloWorld">
+    <container/>
+  </div>
+</template>
+
+<script>
+import container from './Container'
+export default {
+  name: 'HelloWorld',
+  components: {container},
+  props: {
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Muli&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Muli", sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+}
+
+</style>
---
+++ expanding-cards/vue/src/components/Panel.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    :class="{ panel: true, active: active }"
+    @click="itemClick(index)"
+    :style="{ 'background-image': 'url(' + url + ')' }"
+  >
+    <h3>{{ text }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.$emit("itemClick", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.panel {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 80vh;
+  border-radius: 50px;
+  color: #fff;
+  cursor: pointer;
+  flex: 0.5;
+  margin: 10px;
+  position: relative;
+  -webkit-transition: all 700ms ease-in;
+}
+
+.panel h3 {
+  font-size: 24px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  margin: 0;
+  opacity: 0;
+}
+
+.panel.active {
+  flex: 5;
+}
+
+.panel.active h3 {
+  opacity: 1;
+  transition: opacity 0.3s ease-in 0.4s;
+}
+
+@media (max-width: 480px) {
+  .container {
+    width: 100vw;
+  }
+
+  .panel:nth-of-type(4),
+  .panel:nth-of-type(5) {
+    display: none;
+  }
+}
+</style>
